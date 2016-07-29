dbd936d3be2a9941d63b36be62accb06e5838b32
zhanghua <2533058383@qq.com>
2016-07-29 21:23:23 +0800

    test1

--- web/app_test.php
+++ web/app_test.php
@@ -1,4 +1,51 @@
 <?php
+use Symfony\Component\Security\Http\Firewall\ListenerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class SomeAuthenticationListener implements ListenerInterface
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @var AuthenticationManagerInterface
+     */
+    private $authenticationManager;
+
+    /**
+     * @var string Uniquely identifies the secured area
+     */
+    private $providerKey;
+
+    // ...
+
+    public function handle(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        $username = ...;
+        $password = ...;
+
+        $unauthenticatedToken = new UsernamePasswordToken(
+            $username,
+            $password,
+            $this->providerKey
+        );
+
+        $authenticatedToken = $this
+            ->authenticationManager
+            ->authenticate($unauthenticatedToken);
+
+        $this->tokenStorage->setToken($authenticatedToken);
+    }
+}
+
+/*
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 $loader = require __DIR__.'/../app/autoload.php';
@@ -13,6 +60,8 @@ if (in_array($path, array('', '/'))) {
     $response = new Response('Page not found.', Response::HTTP_NOT_FOUND);
 }
 $response->send();
+*/
+
 
 /*
 $request = Request::createFromGlobals();
